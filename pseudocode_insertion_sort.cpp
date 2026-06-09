446b3713944939ac0d4edd844b38bf9703924fc4
abia <47028396+ProTourRial@users.noreply.github.com>
2026-06-09 07:47:49 -0700

    Update pseudocode_insertion_sort.cpp

--- pseudocode_insertion_sort.cpp
+++ pseudocode_insertion_sort.cpp
@@ -1,48 +1,19 @@
-#include <iostream>
-#include <vector>
-using namespace std;
+InsertionSort(A, n)
+    langkah = 0
 
-long long insertionSort(vector<int>& data) {
-    long long langkah = 0;
-    int n = data.size();
+    for i = 1 to n - 1 do
+        key = A[i]
+        j = i - 1
 
-    for (int i = 1; i < n; i++) {
-        int key = data[i];
-        int j = i - 1;
+        while j >= 0 do
+            langkah = langkah + 1
 
-        while (j >= 0) {
-            langkah++;
+            if A[j] > key then
+                A[j + 1] = A[j]
+                j = j - 1
+            else
+                break
 
-            if (data[j] > key) {
-                data[j + 1] = data[j];
-                j--;
-            } else {
-                break;
-            }
-        }
+        A[j + 1] = key
 
-        data[j + 1] = key;
-    }
-
-    return langkah;
-}
-
-int main() {
-    vector<int> data = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
-
-    cout << "Data sebelum diurutkan: ";
-    for (int x : data) {
-        cout << x << " ";
-    }
-
-    long long jumlahLangkah = insertionSort(data);
-
-    cout << "\nData setelah diurutkan: ";
-    for (int x : data) {
-        cout << x << " ";
-    }
-
-    cout << "\nJumlah langkah: " << jumlahLangkah << endl;
-
-    return 0;
-}
+    return langkah
